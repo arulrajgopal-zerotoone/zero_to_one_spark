56c4d1e32783b89fad6e5f1646ca44e0616ad186
arulrajgopal-zerotoone <arulrajgopal-zerotoone>
2024-10-09 10:34:53 +0000

    update

--- databricks_spark/access_adls_through_accesskey.py
+++ databricks_spark/access_adls_through_accesskey.py
@@ -1,10 +1,2 @@
 # Databricks notebook source
-spark.conf.set("fs.azure.account.key.<your-storage-account-name>.dfs.core.windows.net", "<your-access-key>")
-
-# COMMAND ----------
-
-# read a file
-
-df = spark.read\
-    .format("csv")\
-    .
+#read through azure sql
