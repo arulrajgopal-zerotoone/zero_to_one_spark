52501a98e43e9d6083ff23fdfe1d0def8627aeb3
arulrajgopal-zerotoone <arulrajgopal-zerotoone>
2024-10-11 07:56:42 +0000

    update

--- databricks_spark/adls_connectivity.py
+++ databricks_spark/adls_connectivity.py
@@ -65,11 +65,39 @@ dbutils.fs.unmount('/mnt/private')
 
 # COMMAND ----------
 
-spark.conf.set("fs.azure.account.auth.type.arulrajstorageaccount.blob.core.windows.net", "OAuth")
-spark.conf.set("fs.azure.account.oauth.provider.type.arulrajstorageaccount.blob.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
-spark.conf.set("fs.azure.account.oauth2.client.id.arulrajstorageaccount.blob.core.windows.net", "<application-id>")
-spark.conf.set("fs.azure.account.oauth2.client.secret.arulrajstorageaccount.blob.core.windows.net", "<client-secret>")
-spark.conf.set("fs.azure.account.oauth2.client.endpoint.arulrajstorageaccount.blob.core.windows.net", "https://login.microsoftonline.com/<tenant-id>/oauth2/token")
+application_id = "b2bff165-f357-48bb-9553-ffae73e88296"
+tenant_id = "0f46c550-7a7d-4630-b67d-3cb63948aa0a"
+client_secret = "client_id"
+storage_account = "arulrajstorageaccount"
+
+# COMMAND ----------
+
+spark.conf.set(f"fs.azure.account.auth.type.{storage_account}.dfs.core.windows.net", "OAuth")
+spark.conf.set(f"fs.azure.account.oauth.provider.type.{storage_account}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+spark.conf.set(f"fs.azure.account.oauth2.client.id.{storage_account}.dfs.core.windows.net", f"{application_id}")
+spark.conf.set(f"fs.azure.account.oauth2.client.secret.{storage_account}.dfs.core.windows.net", f"{client_secret}")
+spark.conf.set(f"fs.azure.account.oauth2.client.endpoint.{storage_account}.dfs.core.windows.net", f"https://login.microsoftonline.com/{tenant_id}/oauth2/token")
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType
+
+schema = StructType([
+  StructField("id", IntegerType(), True),
+  StructField("name", StringType(), True),
+  StructField("emp_city", StringType(), True)
+])
+
+# COMMAND ----------
+
+read_df = spark.read\
+        .format("csv")\
+        .option("Header",True)\
+        .schema(schema)\
+        .option("sep", "~")\
+        .load("abfss://private@arulrajstorageaccount.dfs.core.windows.net/sample_employee/emp_details/emp_details.csv")
+
+read_df.display()
 
 # COMMAND ----------
 
