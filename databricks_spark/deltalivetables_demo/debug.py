39bb6d9c0505ddeb350043569354658c438b207e
arulrajgopal-zerotoone <arulrajgopal-zerotoone>
2024-10-22 17:27:35 +0000

    update

---
+++ databricks_spark/deltalivetables_demo/debug.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC select * from demo_catalog.demo_databse.stage_employee
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from demo_catalog.demo_databse.curated_employee
