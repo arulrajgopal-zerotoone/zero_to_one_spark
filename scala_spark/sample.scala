1c1ff2be7afdd9334097264f31234e804cc6d89a
arulrajgopal-zerotoone <arulrajgopal-zerotoone>
2024-10-27 08:14:45 +0000

    update

--- scala_spark/sample.scala
+++ scala_spark/sample.scala
@@ -98,7 +98,18 @@ display(final_df)
 
 // COMMAND ----------
 
+// val read_df = spark.readStream.format("cloudFiles")
+//                       .option("cloudFiles.format","parquet")
+//                       .option("cloudFiles.useIncrementalListing","auto")
 
+// val add_timestamp_df = read_df.withColumn("load_date",date_format(current_timestamp.cast("String"),"yyyyMMddHH"))
+
+
+// add_timestamp_df.writeStream
+//                 .trigger(Trigger.AvailableNow())
+//                 .option("checkpointLocation", "path")
+//                 .foreachBatch(writeFunction _)
+//                 .start().awaitTermination()
 
 // COMMAND ----------
 
