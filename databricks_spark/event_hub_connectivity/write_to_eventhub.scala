d607aefce0d8fc2462f59d249c0564fe53651f3b
arulrajgopal-zerotoone <arulrajgopal-zerotoone>
2024-11-06 17:00:31 +0000

    update

--- databricks_spark/event_hub_connectivity/write_to_eventhub.scala
+++ databricks_spark/event_hub_connectivity/write_to_eventhub.scala
@@ -1,12 +1,12 @@
 // Databricks notebook source
-import org.apache.spark.sql.functions.{current_timestamp, col}
+import org.apache.spark.sql.functions.{current_timestamp, col, explode, split,to_json, struct}
 import org.apache.spark.sql.types.{StringType, IntegerType, StructField, StructType}
-// import org.apache.spark.eventhubs._
 import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.eventhubs.{ConnectionStringBuilder, EventHubsConf, EventPosition}
 
 // COMMAND ----------
 
-val schema = StructType(Array(
+val emp_schema = StructType(Array(
   StructField("empid", StringType, true),
   StructField("name", StringType, true),
   StructField("designation", StringType, true),
@@ -19,7 +19,7 @@ val schema = StructType(Array(
 
 val df = spark.readStream
               .format("csv")
-              .schema(schema)
+              .schema(emp_schema)
               .option("Header",true)
               .load("/mnt/test/raw/employee_details")
 
@@ -40,22 +40,30 @@ val query = filteredDf
 
 // COMMAND ----------
 
-val df = spark.read.format("delta").load("/mnt/test/curated/employee_details")
-display(df)
 
-// COMMAND ----------
+val endpoint = "sb://arul-event-hub.servicebus.windows.net/"
+val sharedAccessKeyName = "event-hub-policy"
+val sharedAccessKey = ""
+val entityPath = "event-hub-1"
 
-val connectionString = ConnectionStringBuilder("Endpoint=sb://arul-event-hub.servicebus.windows.net/;SharedAccessKeyName=event-hub-policy;SharedAccessKey=;EntityPath=arul-event-hub")
-  .setEventHubName("arul-event-hub")
+val connectionString = ConnectionStringBuilder(s"Endpoint=$endpoint;SharedAccessKeyName=$sharedAccessKeyName;SharedAccessKey=$sharedAccessKey;EntityPath=$entityPath")
+  .setEventHubName(entityPath)
   .build
 
 val eventHubsConf = EventHubsConf(connectionString)
   .setStartingPosition(EventPosition.fromEndOfStream)
 
-val query = filteredDf
+val dfWithBody = filteredDf.withColumn("body", to_json(struct(df.columns.map(col): _*)))
+
+val query = dfWithBody
   .writeStream
   .format("eventhubs")
   .options(eventHubsConf.toMap)
   .trigger(Trigger.ProcessingTime("10 seconds"))
-  .option("checkpointLocation", "/mnt/checkpoints/employee_details")
+  .option("checkpointLocation", "/mnt/test/checkpoints/employee_details")
   .start()
+
+// COMMAND ----------
+
+val df = spark.read.format("delta").load("/mnt/test/curated/employee_details")
+display(df)
