9d5d611f27952a1d8be45bea0519d1632311eb45
arulrajgopal <arulrajgopal@outlook.com>
2025-08-21 18:43:44 +0000

    movie_lens started

---
+++ apache_spark_local_machine/people_dataset_processed.py
@@ -0,0 +1,39 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType
+import os 
+
+spark = SparkSession.builder \
+    .appName("MyApp") \
+    .getOrCreate()
+
+account_key = os.getenv("AZURE_STORAGE_KEY")
+
+spark.conf.set(
+    "fs.azure.account.key.arulrajgopalshare.dfs.core.windows.net",
+    account_key
+)
+
+schema = StructType([
+    StructField("id", IntegerType(), True),
+    StructField("first_name", StringType(), True),
+    StructField("middle_name", StringType(), True),
+    StructField("last_name", StringType(), True),
+    StructField("gender", StringType(), True),
+    StructField("birth_dt", TimestampType(), True),
+    StructField("ssn", StringType(), True),
+    StructField("salary", IntegerType(), True)
+])
+
+
+df = spark.read.format("csv") \
+    .schema(schema) \
+    .load("abfss://kaniniwitharul@arulrajgopalshare.dfs.core.windows.net/people_csv/people.csv")\
+    .selectExpr("*","floor(months_between(current_date(), birth_dt) / 12) as age")
+
+
+grouped_df = df.groupBy("age").count()
+
+grouped_df.write.mode("overwrite").format("parquet") \
+    .save("abfss://kaniniwitharul@arulrajgopalshare.dfs.core.windows.net/people")
+
+
--- apache_spark_local_machine/test.py
+++ apache_spark_local_machine/test.py
@@ -14,27 +14,26 @@ spark.conf.set(
 )
 
 schema = StructType([
-    StructField("id", IntegerType(), True),
-    StructField("first_name", StringType(), True),
-    StructField("middle_name", StringType(), True),
-    StructField("last_name", StringType(), True),
-    StructField("gender", StringType(), True),
-    StructField("birth_dt", TimestampType(), True),
-    StructField("ssn", StringType(), True),
-    StructField("salary", IntegerType(), True)
+    StructField("title", StringType(), True),
+    StructField("directedBy", StringType(), True),
+    StructField("starring", StringType(), True),
+    StructField("dateAdded", StringType(), True),
+    StructField("avgRating", DecimalType(10, 5), True),
+    StructField("imdbId", IntegerType(), True),
+    StructField("item_id", IntegerType(), True)
 ])
 
 
-df = spark.read.format("csv") \
+df = spark.read.format("json") \
     .schema(schema) \
-    .load("abfss://kaniniwitharul@arulrajgopalshare.dfs.core.windows.net/people_csv/people.csv")
+    .load("abfss://kaniniwitharul@arulrajgopalshare.dfs.core.windows.net/movie_lens_dataset_2e9bytes/metadata.json")\
 
     
-
-repartitioned_df = df.coalesce(10)
-
-
-repartitioned_df.write.mode("overwrite").format("parquet") \
-    .save("abfss://kaniniwitharul@arulrajgopalshare.dfs.core.windows.net/people")
-
-
+df.show()
+# title – movie title (84,484 unique titles)
+# directedBy – directors separated by comma (‘,’)
+# starring – actors separated by comma (‘,’)
+# dateAdded – date, when the movie was added to MovieLens
+# avgRating – average rating of a movie on MovieLens
+# imdbId – movie id on the IMDB website (84,661 unique ids)
+# item_id – movie id, which is consistent across files (84,661 unique ids)
