93524c1b576e5dcdfe426e63cd6e8856b8041d0c
ArulrajGopal <122370244+ArulrajGopal@users.noreply.github.com>
2025-09-09 23:20:37 +0530

    Update spark_conf.py

--- spark_conf.py
+++ spark_conf.py
@@ -3,5 +3,9 @@ spark.conf.set("fs.azure.account.key.<your_storage_account_name>.dfs.core.window
 
 
 spark.conf.set("spark.sql.autoBroadcastJoinThreshold", 100 * 1024 * 1024) // 100 MB
+spark.conf.set("spark.sql.broadcastTimeout", "600s")
+
+
 spark.conf.set("spark.sql.adaptive.enabled", "false")
 spark.conf.set("spark.sql.shuffle.partitions", 10)
+
