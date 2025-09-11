144a0ccbac1fcb70af9202dd472c1d2cbbb4ac67
ArulrajGopal <122370244+ArulrajGopal@users.noreply.github.com>
2025-09-11 21:33:29 +0530

    Update spark_conf.py

--- spark_conf.py
+++ spark_conf.py
@@ -8,4 +8,12 @@ spark.conf.set("spark.sql.broadcastTimeout", "600s")
 
 spark.conf.set("spark.sql.adaptive.enabled", "false")
 spark.conf.set("spark.sql.shuffle.partitions", 10)
+spark.conf.set("spark.sql.files.maxPartitionBytes", "1g")
 
+spark.conf.set("spark.executor.memory", "4g") #  Usage: Sets the amount of memory to allocate per Spark executor.
+spark.conf.set("spark.shuffle.file.buffer", "64k") # Usage: Specifies the buffer size for reading and writing shuffle files.
+spark.conf.set("spark.task.maxFailures", "5") # Usage: Defines the maximum number of failures allowed for a single task before the whole job is considered failed.
+spark.conf.set("spark.reducer.maxSizeInFlight", "96m") # Usage: Sets the maximum size of map output data to fetch simultaneously from each reduce task.
+spark.conf.set("spark.databricks.delta.autoCompact", "true") # Usage: Enables automatic compaction of the Delta table files for better performance.
+spark.conf.set("spark.databricks.delta.retentionDurationCheck.enabled", "true") # Usage: Enables or disables retention duration checks during Delta table compaction.
+spark.conf.set("spark.databricks.io.cache.enabled", "true") # Usage: Enables or disables caching for improved I/O performance.
